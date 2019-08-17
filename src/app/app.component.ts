fbf4f997b6155d57407a278f9e8d1f2ee48decc5
Aditya <sadityam123@gmail.com>
2019-08-17 20:08:25 +0530

    'onecityleft'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'multicity2k19';
+  queries = `What about the eliminations of other competitions?
+  Eliminations to all other competitions will be conducted online or during Mood Indigo. Follow our Facebook page facebook.com/iitb.moodindigo/ and Instagram page https://www.instagram.com/iitbombay.moodi/ for regular updates.
+  
+  There is no Multicity in my city. How can I participate?
+  Eliminations will either be conducted online or during Mood Indigo. Follow our Facebook page facebook.com/iitb.moodindigo/ and Instagram page https://www.instagram.com/iitbombay.moodi/  for regular updates.
+  
+  Who can participate in these competitions?
+  Any college student can take part in these competitions. There is no registration fee for participation.
+  
+  What is Multicity Championship?
+  1 college team from every city shall be crowned ‘Multicity Champion’ based on level of their performances.
+  
+  Are on-spot registrations allowed?
+  It is advised that you register in advance to confirm your slot as there are minimum slots available.
+  
+  Can I participate in these competitions directly during Mood Indigo?
+  If there is a Multicity happening in your city, it is advised that you participate in the Multicity eliminations itself. Getting a slot on-ground for these cities will be very difficult.`;
 }
