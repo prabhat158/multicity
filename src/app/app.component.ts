b942d4b34ae8c641950564d888f2fd6a15d190c3
Aditya <sadityam123@gmail.com>
2019-08-19 14:28:25 +0530

    'queriescomp'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -67,25 +67,6 @@ export class AppComponent {
  
 
   title = 'multicity2k19';
-<<<<<<< HEAD
-  queries = `What about the eliminations of other competitions?
-  Eliminations to all other competitions will be conducted online or during Mood Indigo. Follow our Facebook page facebook.com/iitb.moodindigo/ and Instagram page https://www.instagram.com/iitbombay.moodi/ for regular updates.
-  
-  There is no Multicity in my city. How can I participate?
-  Eliminations will either be conducted online or during Mood Indigo. Follow our Facebook page facebook.com/iitb.moodindigo/ and Instagram page https://www.instagram.com/iitbombay.moodi/  for regular updates.
-  
-  Who can participate in these competitions?
-  Any college student can take part in these competitions. There is no registration fee for participation.
-  
-  What is Multicity Championship?
-  1 college team from every city shall be crowned ‘Multicity Champion’ based on level of their performances.
-  
-  Are on-spot registrations allowed?
-  It is advised that you register in advance to confirm your slot as there are minimum slots available.
-  
-  Can I participate in these competitions directly during Mood Indigo?
-  If there is a Multicity happening in your city, it is advised that you participate in the Multicity eliminations itself. Getting a slot on-ground for these cities will be very difficult.`;
-=======
   public auth2:any;
   public googleInit(){
     gapi.load('auth2',()=>{
@@ -123,5 +104,37 @@ export class AppComponent {
   ngAfterViewInit(){
     this.googleInit();
   }
->>>>>>> a5b5549fb6da51869d1ae2368f22a615f0b745b5
+
+  mqueries = `<b>What about the eliminations of other competitions?</b><br>
+  <p>Eliminations to all other competitions will be conducted online or during Mood Indigo. Follow our Facebook page facebook.com/iitb.moodindigo/ and Instagram page https://www.instagram.com/iitbombay.moodi/ for regular updates.</p>
+
+  <b>There is no Multicity in my city. How can I participate?</b><br>
+  <p>Eliminations will either be conducted online or during Mood Indigo. Follow our Facebook page facebook.com/iitb.moodindigo/ and Instagram page https://www.instagram.com/iitbombay.moodi/ for regular updates.</p>
+
+  <b>Who can participate in these competitions?</b><br>
+  <p>Any college student can take part in these competitions. There is no registration fee for participations.</p>
+
+  <b>What is Muliticity Championship?</b><br>
+  <p>1 college team from every city shall be crowned 'Multicity Champion' based on level of their performances.</p>
+
+  <b>Are on-spot registrations allowed?</b><br>
+  <p>It is advised that you register in advance to confirm your slot as there are limited slots available</p>
+  
+  <b>Can I participate in these competitions directly during Mood Indigo?</b><br>
+  <p>If there is a Multicity happening in your city, it is advised that you participate in the Multicity eliminations itelf. Getting a slot on-ground for these cities will be very difficult.</p>`;
+
+  hqueries = `<b>What about the eliminations of other competitions?</b><br>
+  <p>Eliminations to all other competitions will be conducted online or during Mood Indigo. Follow our Facebook page facebook.com/iitb.moodindigo/ and Instagram page https://www.instagram.com/iitbombay.moodi/ for regular updates.</p>
+
+  <b>There is no Multicity in my city. How can I participate?</b><br>
+  <p>Eliminations will either be conducted online or during Mood Indigo. Follow our Facebook page facebook.com/iitb.moodindigo/ and Instagram page https://www.instagram.com/iitbombay.moodi/ for regular updates.</p>
+  
+  <b>Who can participate in Hysteria?</b><br>
+  <p>Anyone can take part in Hysteria. There is no registration fee for participations.</p>
+  
+  <b>Are on-spot registrations allowed?</b><br>
+  <p>No.</p>
+  
+  <b>Can I participate in these competitions directly during Mood Indigo?</b><br>
+  <p>If there is a Multicity happening in your city, it is advised that you participate in the Multicity eliminations itelf. Getting a slot on-ground for these cities will be very difficult.</p>`;
 }
